caf5ed24a379ac558865615625b5a36fc24ce411
geofmureithi <mureithinjuguna@gmail.com>
2025-10-22 15:29:31 +0300

    fix: improve setup

--- benches/sqlite.rs
+++ benches/sqlite.rs
@@ -10,7 +10,7 @@ use apalis_sqlite::SqliteStorage;
  use apalis_core::task_fn::task_fn;
 
 define_backend_bench!("sqlite_in_file", 10000, {
-    let pool = SqlitePool::connect("test.db").await.unwrap();
+    let pool = SqlitePool::connect("/tmp/test.db").await.unwrap();
     let _ = SqliteStorage::setup(&pool).await;
     SqliteStorage::new_with_config(&pool, &apalis_sqlite::Config::default())
 });
