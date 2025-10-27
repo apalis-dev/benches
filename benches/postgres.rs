84f9f8e7f207208fe638b735ef20be267e3b99b9
geofmureithi <mureithinjuguna@gmail.com>
2025-10-27 10:30:19 +0300

    add: postgres benches

---
+++ benches/postgres.rs
@@ -0,0 +1,26 @@
+use apalis_benches::define_backend_bench;
+use apalis_benches::{bench_worker, TestJob};
+use apalis_core::backend::TaskSink;
+use apalis_core::task_fn::task_fn;
+use apalis_postgres::PostgresStorage;
+use apalis_sqlite::Config;
+use criterion::Criterion;
+use criterion::{criterion_group, criterion_main, BenchmarkId};
+use sqlx::PgPool;
+use std::hint::black_box;
+use tokio::runtime::Runtime;
+
+define_backend_bench!("postgres_basic", 10000, {
+    let pool = PgPool::connect(env!("DATABASE_URL")).await.unwrap();
+    let _ = PostgresStorage::setup(&pool).await;
+    PostgresStorage::new_with_config(&pool, &Config::new("benches_basic"))
+});
+
+define_backend_bench!("postgres_with_notify", 10000, {
+    let pool = PgPool::connect(env!("DATABASE_URL")).await.unwrap();
+    let _ = PostgresStorage::setup(&pool).await;
+    PostgresStorage::new_with_notify(&pool, &Config::new("benches_with_notify"))
+});
+
+criterion_group!(benches, postgres_basic, postgres_with_notify);
+criterion_main!(benches);
